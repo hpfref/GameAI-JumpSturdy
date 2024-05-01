d1fcda2e8c49877e22cfaa94ea77b4da7ecaaefe
Hannes Pohnke <152732225+hpfref@users.noreply.github.com>
2024-05-01 16:02:19 +0200

    zuggenerator

--- src/zuggenerator.py
+++ src/zuggenerator.py
@@ -1,18 +1,39 @@
 
 test_FEN = "b0b0b0b0b0b0/1b0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 b"
 
-def legal_moves(fen:str) -> list:
-    """Return legal moves for a given board position
+
+def legal_moves_board(fen) -> list:
+    """Return all legal moves for the current player
 
     Args:
-        fen (str): board position + player
+        fen: board & player
 
     Returns:
-        list: legal moves
+        list: legal moves e.g. B1-B2, ...
     """
+    board, player = fen.split(" ")
     moves = []
-
     
+
+
 
     return moves
 
+
+def legal_moves_piece(fen, piece) -> list:
+    """Return all legal moves of a single piece
+
+    Args:
+        fen: board & player
+        piece: position of piece to move
+
+    Returns:
+        list: legal moves e.g. B1-B2, ...
+    """
+    board, player = fen.split(" ")
+    moves = []
+    
+
+
+
+    return moves
