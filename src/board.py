02e4a9bb06d3e8ab7099deaf7cf483ef55abd04a
simonselbig <simon.selbig@gmx.de>
2024-05-02 19:48:58 +0200

    figuren ziehen

--- src/board.py
+++ src/board.py
@@ -3,7 +3,8 @@ import zuggenerator as gen
 
 def sz2xy(sz):
   return sz[0]*FELD, sz[1]*FELD
-
+def xy2sz(xy):
+  return xy[0]//FELD, xy[1]//FELD
 
 def zeichneBrett(BRETT):
   for sz, feld in BRETT.items():
@@ -40,27 +41,42 @@ def zeichneFiguren(p):
 pg.init()
 größe = breite, höhe = 800, 800
 FELD = breite // 8
+FPS = 40
 fenster = pg.display.set_mode(größe)
 BRETT = {(s, z): s % 2 == z % 2 for s in range(8) for z in range(8) if (s, z) != (0, 0) and (s, z) != (0, 7) and (s, z) != (7, 0) and (s, z) != (7, 7)}
 fen = 'b0b0b0b0b0b0/1b0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 b'
 fen1 = '3b02/2b2b02/5b0b1/2r0b04/2b3b01/1r1r2r0r0/5r02/2r3 b'
 FIGUREN = ladeFiguren()
-position, zurecht = fen2position(fen1)
+position, zugrecht = fen2position(fen)
 print(position)
-
-
+weitermachen = True
 clock = pg.time.Clock()
-FPS = 40
+drag = None
 
-while True:
+
+
+while weitermachen:
   clock.tick(FPS)
-  fenster.fill('black')
+  for ereignis in pg.event.get():
+    if ereignis.type == pg.QUIT:
+      weitermachen = False
+    elif ereignis.type == pg.MOUSEBUTTONDOWN and not drag:
+      von = xy2sz(pg.mouse.get_pos())
+      if von in position:
+        fig = position[von]
+        drag = FIGUREN[fig]
+        del position[von]
+    elif ereignis.type == pg.MOUSEBUTTONUP and drag:
+      zu = xy2sz(pg.mouse.get_pos())
+      position[zu] = fig
+      drag = None
+
+  fenster.fill((0, 0, 0))
   zeichneBrett(BRETT)
   zeichneFiguren(position)
-  
-  for ereignis in pg.event.get():
-    if ereignis.type == pg.QUIT or ereignis.type == pg.KEYDOWN and ereignis.key == pg.K_ESCAPE: quit()
-  
+  if drag:
+    rect = drag.get_rect(center=pg.mouse.get_pos())
+    fenster.blit(drag, rect)
   pg.display.flip()
 
-
+pg.quit()
