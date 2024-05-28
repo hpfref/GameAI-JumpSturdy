b538e28958936e15f3d766f313fde5691c9df83d
Hannes Pohnke <152732225+hpfref@users.noreply.github.com>
2024-05-28 18:48:29 +0200

    tests & minmax

--- src/gamestate.py
+++ src/gamestate.py
@@ -125,7 +125,7 @@ def evaluate(board):
     red_moves = legal_moves(board, 'r')
     blue_moves = legal_moves(board, 'b')
 
-    helper_eval = {"Position": 0, "Material": 0, "Mobility": 0, "Win": 0} # to understand components
+    #helper_eval = {"Position": 0, "Material": 0, "Mobility": 0, "Win": 0} # to understand components
 
     for row in range(8):
         for col in range(8):
@@ -134,45 +134,45 @@ def evaluate(board):
 
                 # Position ((1.5**row) * 0.2) + Materialwert + ?
                 if piece == 'r':
-                    helper_eval['Position'] -= ((1.5**row) * 0.2)
-                    helper_eval['Material'] -= 1
+                    #helper_eval['Position'] -= ((1.5**row) * 0.2)
+                    #helper_eval['Material'] -= 1
                     value -= ((1.5**row) * 0.2) + 1
 
                 elif piece == 'b':
-                    helper_eval['Position'] += ((1.5**(7 - row)) * 0.2)
-                    helper_eval['Material'] += 1
+                    #helper_eval['Position'] += ((1.5**(7 - row)) * 0.2)
+                    #helper_eval['Material'] += 1
                     value += ((1.5**(7 - row)) * 0.2) + 1
 
                 elif piece == 'rr':
-                    helper_eval['Position'] -= ((1.5**row) * 0.3)
-                    helper_eval['Material'] -= 2
+                    #helper_eval['Position'] -= ((1.5**row) * 0.3)
+                    #helper_eval['Material'] -= 2
                     value -= ((1.5**row) * 0.3) + 2
 
                 elif piece == 'bb':
-                    helper_eval['Position'] += ((1.5**(7 - row)) * 0.3)
-                    helper_eval['Material'] += 2
+                    #helper_eval['Position'] += ((1.5**(7 - row)) * 0.3)
+                    #helper_eval['Material'] += 2
                     value += ((1.5**(7 - row)) * 0.3) + 2
 
                 elif piece == 'br': 
-                    helper_eval['Position'] -= ((1.5**row) * 0.2)
+                    #helper_eval['Position'] -= ((1.5**row) * 0.2)
                     value -= ((1.5**row) * 0.2) + 0 #mby troll aktuell gibts hier nur punkte für rot und materialwert von b und r gleichen sich aus -> durch faktor steuern 
 
                 elif piece == 'rb':
-                    helper_eval['Position'] += ((1.5**row) * 0.2) # same
+                    #helper_eval['Position'] += ((1.5**row) * 0.2) # same
                     value += ((1.5**(7 - row)) * 0.2) + 0
 
                 # Überprüfen, ob ein rotes Stück auf der letzten Reihe ist
                 if row == 7 and piece in ['r', 'rr', 'br']:
-                    helper_eval['Win'] = float('-inf')
+                    #helper_eval['Win'] = float('-inf')
                     return float('-inf')  # Red wins
 
                 # Überprüfen, ob ein blaues Stück auf der ersten Reihe ist
                 if row == 0 and piece in ['b', 'bb', 'rb']:
-                    helper_eval['Win'] = float('inf')
+                    #helper_eval['Win'] = float('inf')
                     return float('inf')  # Blue wins
 
     # Mobilität berücksichtigen
-    helper_eval['Mobility'] = 0.1 * len(blue_moves) - 0.1 * len(red_moves)
+    #helper_eval['Mobility'] = 0.1 * len(blue_moves) - 0.1 * len(red_moves)
     value -= 0.1 * len(red_moves)
     value += 0.1 * len(blue_moves)
 
@@ -183,23 +183,28 @@ def evaluate(board):
 
 def alpha_beta_search(board, player, depth, alpha, beta, maximizing_player, start_time, max_time):
     if time.time() - start_time > max_time:
-        return None, None, False  # Return False to indicate that the search was not completed
+        return None, None, False, 0  # Return False to indicate that the search was not completed
+    
+    nodes_explored = 0 # for testing
 
     moves = legal_moves(board, player)
     if not moves or depth == 0 or game_over(board, player):
+        nodes_explored += 1
         if maximizing_player:
-            return evaluate(board), None, True
-        else: return evaluate(board), None, True
-        #return evaluate(board), None, True  # Return True to indicate that the search was completed
+            return evaluate(board), None, True, nodes_explored # to compare eval functions here
+        else: return evaluate(board), None, True, nodes_explored
+        
+    nodes_explored += 1 # for testing 
 
     if maximizing_player:
         max_eval = float('-inf')
         best_move = None
         for move in moves:
             new_board, new_player = generate_new_board(board, player, move)
-            eval, _, completed = alpha_beta_search(new_board, new_player, depth - 1, alpha, beta, False, start_time, max_time)
+            eval, _, completed, child_nodes_explored = alpha_beta_search(new_board, new_player, depth - 1, alpha, beta, False, start_time, max_time)
+            nodes_explored += child_nodes_explored
             if not completed:
-                return None, None, False  # Return False to indicate that the search was not completed
+                return None, None, False, nodes_explored  # Return False to indicate that the search was not completed
             if eval > max_eval:
                 max_eval = eval
                 best_move = move
@@ -209,15 +214,16 @@ def alpha_beta_search(board, player, depth, alpha, beta, maximizing_player, star
             #print(f"Time elapsed for move {move}: {time.time() - start_time}")
         if best_move == None:
             best_move = moves[0]
-        return max_eval, best_move, True  # Return True to indicate that the search was completed
+        return max_eval, best_move, True, nodes_explored  # Return True to indicate that the search was completed
     else:
         min_eval = float('inf')
         best_move = None
         for move in moves:
             new_board, new_player = generate_new_board(board, player, move)
-            eval, _, completed = alpha_beta_search(new_board, new_player, depth - 1, alpha, beta, True, start_time, max_time)
+            eval, _, completed, child_nodes_explored = alpha_beta_search(new_board, new_player, depth - 1, alpha, beta, True, start_time, max_time)
+            nodes_explored += child_nodes_explored
             if not completed:
-                return None, None, False  # Return False to indicate that the search was not completed
+                return None, None, False, nodes_explored  # Return False to indicate that the search was not completed
             if eval < min_eval:
                 min_eval = eval
                 best_move = move
@@ -226,17 +232,21 @@ def alpha_beta_search(board, player, depth, alpha, beta, maximizing_player, star
                 break
         if best_move == None:
             best_move = moves[0]
-        return min_eval, best_move, True  # Return True to indicate that the search was completed
+        return min_eval, best_move, True, nodes_explored  # Return True to indicate that the search was completed
 
-def iterative_deepening_alpha_beta_search(board, player, max_time, maximizing_player):
+def iterative_deepening_alpha_beta_search(board, player, max_time, max_depth, maximizing_player):
     start_time = time.time()
     depth = 1
     best_move = None
     best_value = float('-inf') if maximizing_player else float('inf')
+    total_nodes_explored = 0 # for testing
 
     while True:
+        if depth > max_depth:
+            break
         print(f"Searching depth {depth}")
-        value, move, completed = alpha_beta_search(board, player, depth, float('-inf'), float('inf'), maximizing_player, start_time, max_time)
+        value, move, completed, nodes_explored = alpha_beta_search(board, player, depth, float('-inf'), float('inf'), maximizing_player, start_time, max_time)
+        total_nodes_explored += nodes_explored
         if not completed:
             break  # Break out of the loop if the search was not completed
         best_value = value
@@ -246,13 +256,99 @@ def iterative_deepening_alpha_beta_search(board, player, max_time, maximizing_pl
         if (maximizing_player and best_value == float('inf')) or (not maximizing_player and best_value == float('-inf')):
             break
 
-    print(best_value)
-    return best_move, depth-1
+    print(f"Best value: {best_value}, Total nodes explored: {total_nodes_explored}")
+    return best_move, depth-1, total_nodes_explored
 
 def select_move(fen):
-    max_time = 4  # Maximum time in seconds for each move
+    max_time = 100  # Maximum time in seconds for each move
+    max_depth = 4  # for testing
     board, player = fen_to_board(fen)
     maximizing_player = player == 'b'
-    best_move, searched_depth = iterative_deepening_alpha_beta_search(board, player, max_time, maximizing_player)
-    print(f"Best move: {best_move}, Depth: {searched_depth}")
+    best_move, searched_depth, nodes_explored = iterative_deepening_alpha_beta_search(board, player, max_time, max_depth, maximizing_player)
+    print(f"Best move: {best_move}, Depth: {searched_depth}, Nodes explored: {nodes_explored}")
+    return best_move
+
+
+
+############################################################## Einfacher MinMax (ohne Cutoffs)
+def min_max_search(board, player, depth, maximizing_player, start_time, max_time):
+    nodes_explored = 0
+
+    def min_max_recursive(board, player, depth, maximizing_player):
+        nonlocal nodes_explored
+        if time.time() - start_time > max_time:
+            return None, None, False  # Return False to indicate that the search was not completed
+
+        if depth == 0 or game_over(board, player):
+            nodes_explored += 1
+            return evaluate(board), None, True  # Return True to indicate that the search was completed
+
+        moves = legal_moves(board, player)
+        if not moves:
+            nodes_explored += 1
+            return evaluate(board), None, True  # Return True to indicate that the search was not completed
+
+        nodes_explored += 1  # Increment the counter for each node
+
+        if maximizing_player:
+            max_eval = float('-inf')
+            best_move = None
+            for move in moves:
+                new_board, new_player = generate_new_board(board, player, move)
+                eval, _, completed = min_max_recursive(new_board, new_player, depth - 1, False)
+                if not completed:
+                    return None, None, False  # Return False to indicate that the search was not completed
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = move
+            return max_eval, best_move, True  # Return True to indicate that the search was completed
+        else:
+            min_eval = float('inf')
+            best_move = None
+            for move in moves:
+                new_board, new_player = generate_new_board(board, player, move)
+                eval, _, completed = min_max_recursive(new_board, new_player, depth - 1, True)
+                if not completed:
+                    return None, None, False  # Return False to indicate that the search was not completed
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = move
+            return min_eval, best_move, True  # Return True to indicate that the search was completed
+
+    value, best_move, completed = min_max_recursive(board, player, depth, maximizing_player)
+    return value, best_move, completed, nodes_explored
+
+def iterative_deepening_min_max_search(board, player, max_time, max_depth, maximizing_player):
+    start_time = time.time()
+    depth = 1
+    best_move = None
+    best_value = float('-inf') if maximizing_player else float('inf')
+    total_nodes_explored = 0
+
+    while True:
+        if depth > max_depth:
+            break
+        print(f"Searching depth {depth}")
+        value, move, completed, nodes_explored = min_max_search(board, player, depth, maximizing_player, start_time, max_time)
+        total_nodes_explored += nodes_explored
+        if not completed :
+            break  # Break out of the loop if the search was not completed
+        best_value = value
+        best_move = move
+        depth += 1
+
+        if (maximizing_player and best_value == float('inf')) or (
+                not maximizing_player and best_value == float('-inf')):
+            break
+
+    print(f"Best value: {best_value}, Total nodes explored: {total_nodes_explored}")
+    return best_move, depth - 1, total_nodes_explored
+
+def select_min_max_move(fen):
+    max_time = 100  # Maximum time in seconds for each move
+    max_depth = 4  # for testing
+    board, player = fen_to_board(fen)
+    maximizing_player = player == 'b'
+    best_move, searched_depth, nodes_explored = iterative_deepening_min_max_search(board, player, max_time, max_depth, maximizing_player)
+    print(f"Best move: {best_move}, Depth: {searched_depth}, Nodes explored: {nodes_explored}")
     return best_move
---
+++ tests/test_mst2.py
@@ -0,0 +1,56 @@
+### To import from src:
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), 'src')))
+###
+import time
+import unittest
+import timeit
+from board import fen_to_board
+from gamestate import select_move, select_min_max_move, evaluate
+
+
+def test_function_runtime(func, *args, **kwargs):
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    runtime = end_time - start_time
+    return result, runtime
+
+def test_search_algorithms(fen):
+    # Test Alpha-Beta Search
+    print("Testing Alpha-Beta Search")
+    best_move_ab, runtime_ab = test_function_runtime(select_move, fen)
+    print(f"Alpha-Beta Best Move: {best_move_ab}, Runtime: {runtime_ab:.4f} seconds")
+
+    # Test Min-Max Search
+    print("Testing Min-Max Search")
+    best_move_mm, runtime_mm = test_function_runtime(select_min_max_move, fen)
+    print(f"Min-Max Best Move: {best_move_mm}, Runtime: {runtime_mm:.4f} seconds")
+
+    return best_move_ab, runtime_ab, best_move_mm, runtime_mm
+
+# Benchmark time of Zuggenerator execution
+def test_evaluate(fen, reps, evaluate):
+    # Measurement of Execution Time and Calculation of Average
+    board, _ = fen_to_board(fen)
+    time_total = timeit.repeat("evaluate(board)", globals=locals(), number=1, repeat=reps)
+    time_avg = sum(time_total) / reps
+    return time_avg
+
+
+
+if __name__ == "__main__":
+    # FENs to test
+    fen_early = "b0b0b0b0b0b0/1b0b0b0b0b0b01/8/8/8/8/1r0r0r0r0r0r01/r0r0r0r0r0r0 b"
+    fen_mid = "b0b0b01bb1/2b0b0bbb02/5r02/3b04/4r0b02/8/2rrr01r02/r0r0r0r01r0 r"
+    fen_late = "3b02/5r02/3r04/8/8/2b02b02/2r05/6 b"
+
+    # alpha beta / minmax
+    #test_search_algorithms(fen)
+
+
+    reps = 1000
+    print("Average time early game (in ms):", test_evaluate(fen_early,reps,evaluate)*1000)
+    print("Average time mid game (in ms):", test_evaluate(fen_mid,reps,evaluate)*1000)
+    print("Average time late game (in ms):", test_evaluate(fen_late,reps,evaluate)*1000)
